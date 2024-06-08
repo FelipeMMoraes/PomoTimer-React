ab3175fee84ad9753d07860b71a9a8a97607db45
Felipe <felipe.moraes8@hotmail.com>
2024-06-08 11:14:28 -0300

    feat: HomePage

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="">I'm going to work in</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="">during</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  );
+}
